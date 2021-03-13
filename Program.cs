600d33355f6c4c332110d9f416a7e0860d8c00d9
Dajour Wilson <75400416+ruojad97@users.noreply.github.com>
2021-03-13 14:26:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DraftRandomizer
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int count = 1;
+            string[] DraftMembers = { "Bandari", "Corey", "Demar", "Jelani", "Jhuvy", "Kheemy", "Kishane", "Raheem" }; //Array containing participants of draft
+            Shuffle(DraftMembers); //Calling Shuffle function
+            foreach (string value in DraftMembers) //foreach loop to display final order of participants
+            {
+                Console.WriteLine("Number " + count + " pick is " + value);
+                count++;
+            }
+        }
+   
+        public static void Shuffle <RandArray>(RandArray[] arr) //Shuffles array at random 
+        {
+            int n = arr.Length;
+            Random num = new Random();
+            for (int i =0; i < n; i++)
+            {
+                int x = i + num.Next(n - i);
+                RandArray temp = arr[x];
+                arr[x] = arr[i];
+                arr[i] = temp;
+            }
+          
+        } 
+    }
+    
+}
